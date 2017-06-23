06aba0c6d2f4048e795531fa81d8cc18876ddac2
Ahmed Şeref GÜNEYSU <serefguneysu@gmail.com>
2017-06-23 23:07:09 +0300

    hot module reload, webpack server etc

---
+++ Makefile
@@ -0,0 +1,12 @@
+default: build
+
+build:
+	@npm run build
+
+watch:
+	@npm run watch	
+
+start:
+	@npm run start
+
+.PHONY: default build watch start
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin() // Enable HMR
+  ],
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+
+  module: {
+  	rules: [
+  		{
+  			test: /\.css$/,
+  			use: [
+  				'style-loader',
+  				'css-loader'
+  			]
+  		}
+  	]
+  },
+
+  devServer: {
+    hot: true, // Tell the dev-server we're using HMR
+    contentBase: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  }  
 };
